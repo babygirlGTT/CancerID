93eac52d8ac251be5b58fcd870a889b0aca79255
znue <2456305444@qq.com>
2017-05-09 15:29:44 +0800

    data

--- CancerID/item_dict.py
+++ CancerID/item_dict.py
@@ -5,27 +5,34 @@ import json
 
 #与列表数值比较并替换
 def compare(ls, num):
-    if num < ls[0]:
-        ls[0] = num
-    elif num > ls[1]:
-        ls[1] = num
+    flag = 0
+    for n in ls[1]:
+        if n == num:
+            flag = 1
+            break
+    if flag:
+        ls[1].append(num)
+        if num < ls[0][0]:
+            ls[0][0] = num
+        elif num > ls[1]:
+            ls[0][1] = num
     return ls
 
-def item():
-    dic = {}
+def dict_gen(dic, f, ):
     with open('/home/zn/Documents/CHARTEVENTS.csv', 'rb') as f:
         reader = csv.DictReader(f)
         for row in reader:
             if row['VALUENUM']: #判断VALUENUM是否为空
                 if row['ITEMID'] in dic.keys():   #判断是否存在于字典中
-                    dic[row['ITEMID']] = compare(dic[row['ITEMID']], float(row['VALUE']))   #比较数值
+                    dic[row['ITEMID']] = compare(dic[row['ITEMID']], float(row['VALUE']))   #比较数值，更新字典
                 else:
-                    dic[row['ITEMID']] = [float(row['VALUE']), float(row['VALUE'])]   #存入字典
-    with open('/home/zn/Desktop/data/item_dict.json', 'wb') as f:
-        json.dump(dic, f)   #存入json文件
+                    dic[row['ITEMID']] = [[float(row['VALUE']), float(row['VALUE'])], [float(row['VALUE'])]]   #存入字典
+    return dic
 
 def main():
-    item()
+    dic = dict_gen()
+    with open('/home/zn/Desktop/data/item_dict.json', 'wb') as f:
+        json.dump(dic, f)   #存入json文件
 
 if __name__ == '__main__':
     main()
