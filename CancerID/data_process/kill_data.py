e12ab5cba17bad207a9131ec45a9b5e20e766c13
znue <2456305444@qq.com>
2017-05-11 15:53:58 +0800

    dict intervals

--- CancerID/data_process/kill_data.py
+++ CancerID/data_process/kill_data.py
@@ -4,8 +4,8 @@
 import os
 import pandas as pd
 
-DIR_PATH = "/home/andrew/Desktop/test/"
-OUT_PATH = "/home/andrew/Desktop/"
+DIR_PATH = "/media/zn/00051A0B00093CEE/mimic/"
+OUT_PATH = "/home/zn/Desktop/"
 
 def read_files(path):
     ''' return file list '''
@@ -16,7 +16,7 @@ def read_files(path):
     return f_ls
 
 FILENAMES = read_files(DIR_PATH)
-j = 0
+
 for filename in FILENAMES:
     store_index = set([1])
     with open(filename, "r") as fr:
@@ -33,16 +33,11 @@ for filename in FILENAMES:
                 store_index.add(index)
             elif item == 'INTERPRETATION':
                 store_index.add(index)
-    j += 1
-    fw = open(OUT_PATH + str(j) + '.csv', 'w')
-    print [head_list[i] for i in store_index]
-    fw.writelines(','.join([head_list[i] for i in store_index]) + '\n')
-    df = pd.read_csv(filename, dtype='string', chunksize=10000)
+        fw = open(OUT_PATH + filename[-25:], 'w')
+        print [head_list[i] for i in store_index]
+        fw.writelines(','.join([head_list[i] for i in store_index]) + '\n')
+        fw.close()
+    df = pd.read_csv(filename, dtype='string', chunksize=50000)
     for chunk in df:
         header = [chunk.columns[i] for i in store_index]
-        chunk.loc[:, header].to_csv(OUT_PATH + str(j) + '.csv', mode='a', header=False, index=False)
-        # for line in fr:
-        #     data_list = line[:-1].replace('"', '').split(',')
-        #     print data_list
-        #     line_to_write = ','.join([data_list[i] for i in store_index]) + '\n'
-        #     fw.writelines(line_to_write)
+        chunk.loc[:, header].to_csv(OUT_PATH + filename[-25:], mode='a', header=False, index=False)
--- CancerID/item_dict.py
+++ CancerID/item_dict.py
@@ -5,13 +5,13 @@ import json
 import time
 
 #与列表数值比较并替换
-def compare(ls, num):
-    ls[1].add(num)
-    if num < ls[0][0]:
-        ls[0][0] = num
-    elif num > ls[1]:
-        ls[0][1] = num
-    return ls
+def compare(d, num):
+    d['nums'].add(num)
+    if num < d['min']:
+        d['min'] = num
+    elif num > d['max']:
+        d['max'] = num
+    return d
 
 def dict_gen(dic, fn, id='ITEMID',key='VALUENUM'):
     with open(fn, 'rb') as f:
@@ -22,10 +22,10 @@ def dict_gen(dic, fn, id='ITEMID',key='VALUENUM'):
                     dic[row[id]] = compare(dic[row[id]], float(row[key]))   #比较数值，更新字典
                 else:
                     s = set([float(row[key])])
-                    dic[row[id]] = [[float(row[key]), float(row[key])], s]   #存入字典
+                    dic[row[id]] = {'min':float(row[key]), 'max':float(row[key]), 'nums':s}   #存入字典
     return dic
 
-def dict_gen(dic, f, value):
+def dict_gen_(dic, f, value):
     with open('/home/zn/Documents/CHARTEVENTS.csv', 'rb') as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -46,23 +46,18 @@ def main():
     e = time.time()
     print e-s
 
-    fn = '/home/zn/Documents/mimic/LABEVENTS_DATA_TABLE.csv'
+    fn = '/home/zn/Desktop/LABEVENTS_DATA_TABLE.csv'
     dic = dict_gen(dic, fn)
     e = time.time()
     print e-s
 
-    fn = '/home/zn/Documents/mimic/MICROBIOLOGYEVENTS_DATA_TABLE.csv'
-    dic = dict_gen(dic, fn, id='SPEC_ITEMID', key='DILUTION_VALUE')
-    e = time.time()
-    print e-s
-
-    fn = '/home/zn/Documents/mimic/OUTPUTEVENTS_DATA_TABLE.csv'
+    fn = '/home/zn/Desktop/OUTPUTEVENTS_DATA_TABLE.csv'
     dic = dict_gen(dic, fn, key='VALUE')
     e = time.time()
     print e-s
 
     for key in dic.keys():
-        dic[key] = [dic[key][0], list(dic[key][1])]
+        dic[key]['nums'] = list(dic[key]['nums'])
     with open('/home/zn/Desktop/data/item_dict.json', 'wb') as f:
         json.dump(dic, f)   #存入json文件
 
